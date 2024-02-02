d00e826a9b9057ee968726d5b495076cd0cf7625
Artem Zhmura <99594471+Somnovem@users.noreply.github.com>
2024-02-02 13:30:34 +0200

    Logout path added

--- back/api/src/routes/api.php
+++ back/api/src/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
+Route::post('auth/logout', LogoutController::class);
